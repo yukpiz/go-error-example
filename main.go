d957f9a2c562e56139de531487c2482bfe782b05
yukpiz <yukpiz@gmail.com>
2019-06-14 17:43:16 +0900

    up

--- main.go
+++ main.go
@@ -1,15 +1,41 @@
 package main
 
+import (
+	"fmt"
+	"log"
+
+	"golang.org/x/xerrors"
+)
+
 func main() {
+	err := ErrorFn()
+	log.Printf("%v\n", err)
+	log.Printf("%+v\n", err)
+	fmt.Printf("%v\n", err)
+	fmt.Printf("%+v\n", err)
 }
 
-func Application() {
+func ErrorFn() error {
+	return ErrorCodes.ErrorType1.Error
 }
 
-func BusinessLogic() error {
-	return nil
+type customError struct {
+	Error       error
+	Description string
 }
 
-func Repository() error {
-	return nil
+type errs struct {
+	ErrorType1 customError
+	ErrorType2 customError
+	ErrorType3 customError
+	ErrorType4 customError
+	ErrorType5 customError
+}
+
+var ErrorCodes = errs{
+	ErrorType1: customError{Error: xerrors.New("Custom.Error1")},
+	ErrorType2: customError{Error: xerrors.New("Custom.Error2")},
+	ErrorType3: customError{Error: xerrors.New("Custom.Error3")},
+	ErrorType4: customError{Error: xerrors.New("Custom.Error4")},
+	ErrorType5: customError{Error: xerrors.New("Custom.Error5")},
 }
